c40fdb9ea2b3b36e1958a30821727d5b00636f82
Barnold88 <bandslover@outlook.com>
2025-12-02 18:10:11 +0000

    Change loader to check dependencies via pkg_resources to make cleaner

--- src/loader.py
+++ src/loader.py
@@ -1,15 +1,30 @@
-installString = "Try running \"pip install -r requirements.txt\" from the \"contents directory\"."
+import sys
+import subprocess
+import pkg_resources
+from typing import List
 
-def printLibError(lib, installString):
-    print(f"Missing library \"{lib}\". {installString}")
-    exit(1)
+def errorOut(dependencies:List[str]) -> None: 
 
-try:
-    import flask
-except ImportError:
-    printLibError("flask",installString)
+    if len(dependencies) <= 0:
+        return
 
-try:
-    import requests
-except ImportError:
-    printLibError("requests",installString)
+def satisfiedLibs() -> bool:
+
+    required  = None
+    satisfied = True
+    installString = "Try running \"pip install -r requirements.txt\" from the \"contents\" directory."
+
+    with open("../contents/requirements.txt") as requirements:
+        required = requirements.readlines()
+        
+    for package in required:
+        try:
+            dist = pkg_resources.get_distribution(package)
+        except pkg_resources.DistributionNotFound:
+            print(f"Could not find the lib {package}")
+            satisfied = False
+            
+    if satisfied == False:
+        print("\n"+installString)
+
+    return satisfied
