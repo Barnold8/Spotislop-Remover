c6d60a830242c83ff4827a2541986d59eba4fe00
Barnold88 <bandslover@outlook.com>
2025-12-03 23:09:49 +0000

    load in settings from data structure to avoid hardcoding and make the software configurable

--- src/API_Handler.py
+++ src/API_Handler.py
@@ -1,40 +1,35 @@
-from loader import loadContents
 from Url import *
+import data
 import requests
+
 
 def OAuth_Spotify():
 
-    contents = loadContents()
-    redirect_URL = "http://127.0.0.1:5000/result"
+    redirect_URL = data.contents["settings"]["redirects"]["spotify-auth"]
 
     state = generateRandomString(16)
     scope = "playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public" 
 
     body = {
         "response_type": 'code',
-        "client_id": contents["client_id"],
+        "client_id": data.contents["client_id"],
         "scope": scope,
         "redirect_uri": redirect_URL,
         "state": state
     }   
+
     return body
 
 def getAccessToken(OAuthToken:str) -> str:    
 
-    contents = {}
-
-    with open("../contents/CLIENT_ID") as client:
-        contents["client_id"] = client.read()
-    with open("../contents/CLIENT_SECRET") as client:
-        contents["client_secret"] = client.read()
-
     url = "https://accounts.spotify.com/api/token"
+
     request_body = {
         "grant_type": "authorization_code",
         "code": OAuthToken,
         "redirect_uri": "http://127.0.0.1:5000/result",
-        "client_id": contents["client_id"],
-        "client_secret": contents["client_secret"],
+        "client_id": data.contents["client_id"],
+        "client_secret": data.contents["client_secret"],
     }
     
     r = requests.post(url, data=request_body)
