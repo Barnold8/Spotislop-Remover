6c2f26d6a56531453ef29b8f6b98837205523796
Barnold88 <bandslover@outlook.com>
2025-12-04 21:15:24 +0000

    Make generalised user class

--- src/data.py
+++ src/data.py
@@ -1,5 +1,33 @@
 import loader
 from typing import List
+from datetime import datetime,timedelta
+
+import sys
+
+
+class User:
+
+    def __init__(self,userPayload: dict):
+
+        print(userPayload,sys.stderr)
+
+        self.access_token  = userPayload["access_token"]
+        self.refresh_token = userPayload["refresh_token"]
+        self.token_type    = userPayload["token_type"]
+        self.expiration    = User.getExpirationDateTime(userPayload["expires_in"])
+
+    def getExpirationDateTime(seconds: int):
+
+        now = datetime.today()
+        expiration = now + timedelta(0,seconds)
+
+    def toJson(user: 'User') -> dict:
+        pass
+
+    def toUser(json:dict) -> 'User':
+        pass
+
+
 
 def compileScopes(scopes: List[str]) -> str:
 
