d04a9ddf43be2c1314b5d6258a001280aa8791c9
Barnold88 <bandslover@outlook.com>
2025-12-04 18:59:28 +0000

    Add scopes to config file to allow restrictions on user access and include method to turn array of scopes to string

--- src/data.py
+++ src/data.py
@@ -1,3 +1,8 @@
 import loader
+from typing import List
+
+def compileScopes(scopes: List[str]) -> str:
+
+    return " ".join(x for x in scopes)
 
 contents = loader.loadContents() # this is on a wider scope to avoid reading from disk everytime a user needs to interact with the flask server
