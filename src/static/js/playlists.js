57912197ca1c6b2e1946de680fd42acf42353bcc
Barnold88 <bandslover@outlook.com>
2025-12-09 21:15:39 +0000

    Add toasting logic to be useful around website and disallow same playlist to be added again

--- src/static/js/playlists.js
+++ src/static/js/playlists.js
@@ -12,21 +12,73 @@ function fillDescriptions(){
     }
 }
 
-fillDescriptions()
 
+function alreadyInList(element,list){
+
+    for (let i = 0; i < list.length; i++) {
+        if(list[i] === element){
+            return true
+        }
+    }
+    
+    return false;
+}
+
+function toast(message,seconds) {
+  // Get the snackbar DIV
+  var x = document.getElementById("snackbar");
+
+  x.innerText = message
+
+  // Add the "show" class to DIV
+  x.className = "show";
+
+  // After 3 seconds, remove the show class from DIV
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, seconds * 1000);
+}
+
+function grabListItems(id,tag){
+
+    array = []
+    list = document.getElementById(id)
+    list = list.getElementsByTagName(tag)
+
+    for (let i = 0; i < list.length; i++) {
+        array.push(list[i].innerText)
+    }
+
+    return array
+
+}
 
 function addToCart(name){
 
-    const newListItem = document.createElement("li");
-    const newLink = document.createElement("a");
-    const newLabel = document.createElement("label");
+    if(!alreadyInList(name,grabListItems("menu","li"))){
+        
+        toast(`Added ${name}`,3)
 
-    cart = document.getElementById("menu")
+        const newListItem = document.createElement("li");
+        const newLink = document.createElement("a");
+        const newLabel = document.createElement("label");
 
-    newLabel.innerText = name
+        cart = document.getElementById("menu")
 
-    newLink.appendChild(newLabel)
-    newListItem.appendChild(newLink)
-    cart.appendChild(newListItem)
+        newLabel.innerText = name
+
+        newLink.appendChild(newLabel)
+        newListItem.appendChild(newLink)
+        cart.appendChild(newListItem)
+
+    }else{
+        toast(`${name} is already in the list`,3)
+    }
 
 }
+
+function scanPlaylists(){
+
+    // Redirect to route that scans playlists and package list in some data structure to route
+
+}
+
+fillDescriptions()
