efeae9085c290697d75288c8f4bf0b83cc518d89
Barnold88 <bandslover@outlook.com>
2025-12-11 17:26:32 +0000

    Turn args in url to an array of strings

--- src/data.py
+++ src/data.py
@@ -99,4 +99,7 @@ def combineDicts(*dicts):
 
     return final_dict
 
+def idsToArray(ids:str) -> List[str]:
+    return [id for id in ids.split(",")]
+
 contents = loader.loadContents() # this is on a wider scope to avoid reading from disk everytime a user needs to interact with the flask server
