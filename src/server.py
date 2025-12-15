f974ed380fddd3018a5f0ee98327056a4f89e441
Barnold88 <bandslover@outlook.com>
2025-12-15 00:22:32 +0000

    Add way to check for valid session data as extensively as possible without external network requests

--- src/server.py
+++ src/server.py
@@ -2,6 +2,7 @@ from flask import Flask,redirect,render_template,request,session
 from Url import *
 from API_Handler import *
 from data import User, Playlist, combineDicts,idsToArray
+from datetime import datetime
 import json
 import sys
 
@@ -11,20 +12,50 @@ def validateUserSession() -> bool:
 
     sessionKeys = session.keys()
     expectedUserKeys = ['access_token', 'display_name', 'expires_in', 'profile_picture', 'refresh_token', 'token_type', 'user_id']
+    importantFields = ['access_token', 'display_name', 'expires_in', 'refresh_token', 'user_id']
 
     if "messages" not in sessionKeys:
         return False
+    
     if set(session["messages"].keys()) != set(expectedUserKeys):
         return False
-
+   
     for key, value in session["messages"].items():
         if value == None:
             return False
+        
+        if key in importantFields: # fair bit of duplicate code since its mostly string validation with no specific metrics but it keeps it readable 
+            match key:
+                case "access_token":
+                    token =  session["messages"]["access_token"]
+                    if len(token) <= 0:
+                        return False
+                    
+                case "display_name":
+                    name = session["messages"]["display_name"]
+                    if len(name) > 30 or len(name) <= 0:
+                        return False
+                    
+                case "expires_in":
+                    try:
+                        date = date.fromisoformat(session["messages"]["expires_in"])
+                    except ValueError:
+                        return False
+                    
+                case "refresh_token":
+                    token = session["messages"]["refresh_token"]
+                    if len(token) <= 0:
+                        return False
+
+                case "user_id":
+                    token = session["messages"]["user_id"]
+                    if len(token) <= 0:
+                        return False
 
     return True
 
 def validateUserAuth(userCode: str) -> bool:    # for clarification, this is used to verify the users auth PRE access token grant
-    MAXLEN    = 306
+    MAXLEN = 306
     if userCode == None:
         return False
 
